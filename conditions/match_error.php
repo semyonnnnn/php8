213a05f83c101d561758a43d9c142285b35ca0e8
Семён Максимович Гусев <145036030+semyonnnnn@users.noreply.github.com>
2025-10-02 09:28:46 +0500

    Update match_error.php
    read docs

--- conditions/match_error.php
+++ conditions/match_error.php
@@ -1,7 +1,10 @@
 <?php
 $day = match('PHP') {
-        'Jan', 'Mar', 'May', 'Jul', 'Aug', 'Oct', 'Dec' => 31,
         'Apr', 'Jun', 'Sep', 'Nov' => 30,
         'Feb' => 28,
+        
+        //actually 👆🤓
+        default => 31,
     };
+
 echo $day; // PHP Fatal error: Uncaught UnhandledMatchError: Unhandled match case 'PHP'
